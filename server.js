92fab2477e8193bda8e41afd502770b4a500fb59
Loc Ronaldo <loccb90@gmail.com>
2017-05-20 18:23:06 +0700

    new update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.engine('html', require('ejs').renderFile)
 
-app.use(express.static(path.join(__dirname, "client")))
+app.use(express.static(path.join(__dirname, "clientSrc")))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
